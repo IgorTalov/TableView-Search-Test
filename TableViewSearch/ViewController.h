8ee68d5e67bbbb9484b7ff4924f5fcb2ef39d725
Igor Talov <igortalov@gmail.com>
2016-05-19 16:35:15 +0300

    Initial Commit

---
+++ TableViewSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewSearch
+//
+//  Created by Игорь Талов on 19.05.16.
+//  Copyright © 2016 Игорь Талов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableViewSearch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TableViewSearch
+//
+//  Created by Игорь Талов on 19.05.16.
+//  Copyright © 2016 Игорь Талов. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
